64ba0d9672aef71f81a201c40b545995e9d61467
jedlee <jedlee616@gmail.com>
2026-01-25 02:41:35 +0800

    在现有多模型选择基础上再次新加入cyrillic_PP-OCRv5_rec_mobile_infer，th_PP-OCRv5_rec_mobile_infer，ta_PP-OCRv5_rec_mobile_infer

--- app/src/main/java/hzkitty/android/ocr/MainActivity.java
+++ app/src/main/java/hzkitty/android/ocr/MainActivity.java
@@ -114,6 +114,9 @@ public class MainActivity extends AppCompatActivity {
         modelList.add(new OcrModel("阿拉伯文字识别模型", "arabic_PP-OCRv5_rec_mobile_infer.onnx"));
         modelList.add(new OcrModel("斯拉夫文字识别模型", "eslav_PP-OCRv5_rec_mobile_infer.onnx"));
         modelList.add(new OcrModel("韩文字识别模型", "korean_PP-OCRv5_rec_mobile_infer.onnx"));
+        modelList.add(new OcrModel("西里尔文字识别模型", "cyrillic_PP-OCRv5_rec_mobile_infer.onnx"));
+        modelList.add(new OcrModel("泰文字识别模型", "th_PP-OCRv5_rec_mobile_infer.onnx"));
+        modelList.add(new OcrModel("泰米尔文字识别模型", "ta_PP-OCRv5_rec_mobile_infer.onnx"));
         
         // 设置只有中文模型默认选中，其他模型默认不选中
         for (int i = 1; i < modelList.size(); i++) {
